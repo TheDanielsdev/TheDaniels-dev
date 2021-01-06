979b5963bebed3b4ee3a15fc01ed34f93c463c94
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-06 11:27:16 +0100

    just trying out some stuffs

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function ($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if (!action) {
-      action = 'thedanielsdev@gmail.com';
+      action = 'validate.php';
     }
     $.ajax({
       type: "POST",
