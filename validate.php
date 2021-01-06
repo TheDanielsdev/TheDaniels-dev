a6c2543fe96ccb12f5cef03ac0f4adc778c66433
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-06 09:57:42 +0100

    validating form using php

---
+++ validate.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $msg = $_POST['message'];
+
+
+    $mailTo = "thedanielsdev@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an email from ".$name.".\n\n".$msg;
+
+mail($mailTo, $subject, $txt, $headers);
+header("location: index.html?messagesent");
+
+}
