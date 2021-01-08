6641a5298c503b6538620cc7507be6d6a01fea40
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-08 18:22:18 +0100

    finally was able to validate the form

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function ($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if (!action) {
-      action = '';
+      action = 'https://formsubmit.co/thedanielsdev@gmail.com';
     }
     $.ajax({
       type: "POST",
@@ -116,45 +116,5 @@ jQuery(document).ready(function ($) {
   });
 
 });
-
-
-$(function () {
-  $("#register").validate({
-    rules: {
-      name: {
-        required: true,
-        noSpace: true
-      },
-      email: {
-        required: true,
-        email: true
-      },
-      subject: {
-        required: true,
-        noSpace: true
-
-      },
-      message: {
-        required: true,
-        noSpace: true
-      }
-    },
-    message: {
-      name: {
-        required: 'please enter name!.'
-      },
-      email: {
-        required: 'please enter email!.',
-        email: 'enter valid email'
-      }
-    }
-
-  })
-})
-
-$.validator.addMethod("noSpace", function (value, element) {
-  return value == '' || value.trim().length != 0
-}, "spaces are not allowed");
-
 
 
