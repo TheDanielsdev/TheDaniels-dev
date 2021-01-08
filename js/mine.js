60e2312d49b8f3a85a9351e796d93f439c185dbd
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-08 16:56:56 +0100

    checking form

--- js/mine.js
+++ js/mine.js
@@ -4,3 +4,41 @@ theDaniels.addEventListener('click', function () {
     document.body.classList.toggle('dark-theme');
 })*/
 
+
+$(function () {
+    $("#register").validate({
+        rules: {
+            name: {
+                required: true,
+                noSpace: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true,
+                noSpace: true
+
+            },
+            message: {
+                required: true,
+                noSpace: true
+            }
+        },
+        message: {
+            name: {
+                required: 'please enter name!.'
+            },
+            email: {
+                required: 'please enter email!.',
+                email: 'enter valid email'
+            }
+        }
+
+    })
+})
+
+$.validator.addMethod("noSpace", function (value, element) {
+    return value == '' || value.trim().length != 0
+}, "spaces are not allowed");
