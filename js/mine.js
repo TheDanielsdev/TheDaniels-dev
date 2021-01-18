fe1eceb3e24233b5c008b3f6edb9535f5395e3e6
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-18 12:35:23 +0100

    just added cookies

--- js/mine.js
+++ js/mine.js
@@ -3,10 +3,26 @@
 theDaniels.addEventListener('click', function () {
     document.body.classList.toggle('dark-theme');
 })*/
- 
- function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-theme");
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-theme");
 }
 
 
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+    localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+    if (!localStorage.getItem("cookieBannerDisplayed")) {
+        cookieContainer.classList.add("active");
+    }
+
+
+}, 2000);
