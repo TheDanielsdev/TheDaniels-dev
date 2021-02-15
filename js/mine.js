dd36544709212a8f07029b3576b84cfd275fd328
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-02-15 11:17:47 +0100

    added a portfolio

--- js/mine.js
+++ js/mine.js
@@ -26,16 +26,3 @@ setTimeout(() => {
 
 
 }, 1000);
-
-
-
-// When the user scrolls the page, execute myFunction (for the scroll indicator)
-
-window.onscroll = function() { myFunction() };
-
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-}
